fdea485bfccace1f61097b50a61cf827042f995f
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2008-07-11 16:45:15 +0000

    *** empty log message ***

---
+++ src/main/java/com/idega/app/eplatform/mac/App.java
@@ -0,0 +1,13 @@
+package com.idega.app.eplatform.mac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
